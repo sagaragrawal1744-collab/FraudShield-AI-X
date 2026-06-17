2f8af7e6b3d1bd06cc615a8d23a8785937460034
Sagar Agrawal <sagaragrawal1744@gmail.com>
2026-06-17 19:01:45 +0530

    Day 20 dashboard and prediction history

--- Dataset/app.py
+++ Dataset/app.py
@@ -4,10 +4,13 @@ import joblib
 
 app = Flask(__name__)
 
+# Load model
 model = joblib.load("fraud_model.pkl")
 
+# Load dataset
 data = pd.read_csv("Cleaned_DataSet_V3.csv")
 
+# Convert text columns
 text_columns = data.select_dtypes(include=["object", "str"]).columns
 
 for col in text_columns:
@@ -15,31 +18,50 @@ for col in text_columns:
 
 X = data.drop(["F3912", "F3924"], axis=1)
 
+# Prediction History
+history = []
+
 @app.route("/", methods=["GET", "POST"])
 def home():
 
     result = ""
     row_value = ""
+    risk = ""
 
     if request.method == "POST":
 
         row_value = request.form["row"]
 
-        row = int(row_value)
+        try:
 
-        sample = X.loc[[row]]
+            row = int(row_value)
 
-        prediction = model.predict(sample)
+            sample = X.loc[[row]]
 
-        if prediction[0] == 1:
-            result = "🚨 Fraud Detected"
-        else:
-            result = "✅ Normal Transaction"
+            prediction = model.predict(sample)
+
+            if prediction[0] == 1:
+                result = "🚨 Fraud Detected"
+                risk = "🔴 HIGH RISK"
+
+            else:
+                result = "✅ Normal Transaction"
+                risk = "🟢 LOW RISK"
+
+            history.insert(0, {
+                "row": row,
+                "result": result
+            })
+
+        except:
+            result = "Invalid Transaction ID"
 
     return render_template(
         "index.html",
         result=result,
-        row_value=row_value
+        row_value=row_value,
+        risk=risk,
+        history=history[:10]
     )
 
 if __name__ == "__main__":
